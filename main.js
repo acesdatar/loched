2cd9504d8f605a2d02df45b8903adc2be649673f
Derek Duafa <Derek.Duafa@gcnetghana.com>
2018-11-20 18:09:17 +0000

    This is my website

---
+++ main.js
@@ -0,0 +1,13 @@
+var index = 0;
+carousel();
+
+function carousel() {
+    var x = document.getElementsByClassName("imageSliders");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"
+    }
+    index++;
+    if ( index > x.length) { index = 1}
+    x[index-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
